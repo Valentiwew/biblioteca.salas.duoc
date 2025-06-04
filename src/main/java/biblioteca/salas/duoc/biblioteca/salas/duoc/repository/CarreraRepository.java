53649f38b214ed099ae02ddbcab4f595e04e1a37
fravellog <fr.avellog@duocuc.cl>
2025-06-04 15:47:48 -0400

    repositorio de carrrera (revisar las otras)

--- src/main/java/biblioteca/salas/duoc/biblioteca/salas/duoc/repository/CarreraRepository.java
+++ src/main/java/biblioteca/salas/duoc/biblioteca/salas/duoc/repository/CarreraRepository.java
@@ -1,5 +1,10 @@
 package biblioteca.salas.duoc.biblioteca.salas.duoc.repository;
 
-public class CarreraRepository {
+import biblioteca.salas.duoc.biblioteca.salas.duoc.model.Carrera;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface CarreraRepository extends JpaRepository<Carrera, String> {
+    //Hay que eliminar y reacerlos porque son un interface, no una clase xd
 
 }
