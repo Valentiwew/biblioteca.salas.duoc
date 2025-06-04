d7ace55f6ffb3a4c90255b4e3f128bb870807f3b
fravellog <fr.avellog@duocuc.cl>
2025-06-04 15:47:27 -0400

    Modelo de carrera

--- src/main/java/biblioteca/salas/duoc/biblioteca/salas/duoc/model/Carrera.java
+++ src/main/java/biblioteca/salas/duoc/biblioteca/salas/duoc/model/Carrera.java
@@ -1,5 +1,20 @@
 package biblioteca.salas.duoc.biblioteca.salas.duoc.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "carrera")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Carrera {
+    @Id
+    @Column(nullable = false, length = 50)
+    private String codigo;
 
+    @Column(nullable = false, length = 100)
+    private String nombre;
 }
